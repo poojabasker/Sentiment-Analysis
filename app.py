64b2718e53e1f27ff58da7bb286f290718da0afd
Pooja B <53658738+poojabasker@users.noreply.github.com>
2021-05-18 19:08:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from flask import Flask, jsonify, request
+import pickle
+from keras.models import model_from_json,model_from_yaml
+import clean_data as cd
+import json
+import requests
+
+yaml_file = open('model.yaml', 'r')
+loaded_model_yaml = yaml_file.read()
+yaml_file.close()
+model = model_from_yaml(loaded_model_yaml)
+# load weights into new model
+model.load_weights("model.h5")
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/', methods=['POST'])
+
+def predict():
+    # get data
+    data = request.get_json(force=True)
+    #print('Data received: "{data}"'.format(data=data))
+    #data=json.dumps(d)
+    data = data["text"]
+
+    # predictions
+    result = cd.get_sentiment(model, data)
+    #result = json.dumps(result)
+
+    # send back to browser
+    output = result
+
+    # return data
+    #return data
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug=True)
