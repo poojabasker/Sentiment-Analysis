6817650af27e251bc88c73050e841d0badca68e1
Pooja B <53658738+poojabasker@users.noreply.github.com>
2021-06-04 19:47:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,28 +17,23 @@ model.load_weights("model.h5")
 app = Flask(__name__)
 
 # routes
-@app.route('/', methods=['GET'])
-def home():
-    return ("Hello world!")
-
+# routes
 @app.route('/', methods=['POST'])
 def predict():
     # get data
     data = request.get_json(force=True)
-    #print('Data received: "{data}"'.format(data=data))
-    #data=json.dumps(d)
+   
     data = data["text"]
 
     # predictions
     result = cd.get_sentiment(model, data)
     #result = json.dumps(result)
 
-    # send back to browser
-    output = result
+    temp = sorted(result.items(), key=lambda item: item[1], reverse=True)
+    result = dict(temp[0:3])
 
-    # return data
     #return data
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
